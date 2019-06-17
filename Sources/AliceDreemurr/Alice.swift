85ab693510644f589731ff47c57f1e85fe7d82ad
Marquis Kurt <software@marquiskurt.net>
2019-06-16 21:16:35 -0400

    Add documentation and new commands

--- Sources/AliceDreemurr/Alice.swift
+++ Sources/AliceDreemurr/Alice.swift
@@ -1,22 +1,35 @@
 //
-//  Commands.swift
-//  
+//  Alice.swift
+//  dreemurr-alice
 //
 //  Created by Marquis Kurt on 6/16/19.
+//  (C) 2019 Project Alice. All rights reserved.
 //
 
 import Foundation
 import DreemurrCore
 import Sword
 
+// Override the existing DreemurrCommands to add new commands
 public enum DreemurrCommands {
     case help
     case introduce
     case ping
     case qt
+    case aboutme
 }
 
+/**
+    Base class for Alice Angel bot. Inherits from `Dreemurr` class.
+ */
 public class Alice: Dreemurr {
+    
+    /**
+        Tokenize the command to a valid one.
+        - parameter input: The input to tokenize
+        - returns: The tokenized command as a `DreemurrCommands` enumeration.
+        - throws: Throws `DreemurrError.invalidCommand` when finding an invalid command.
+     */
     public func toAliceCommand(input: String) throws -> DreemurrCommands {
         switch input {
         case "!help":
@@ -27,11 +40,46 @@ public class Alice: Dreemurr {
             return .ping
         case "!qt":
             return .qt
+        case "!aboutme":
+            return .aboutme
         default:
             throw DreemurrError.invalidCommand
         }
     }
     
+    /**
+        Gets a random ping message.
+     */
+    public func getPingMessage() -> String {
+        let possibleEntries = [
+            "I would appreciate if you did not.",
+            "I'm a bot! Congratulations.",
+            "What, do you think I'm going to say 'Pong' to that?",
+            "Please don't.",
+            "I'm busy right now."
+        ]
+        return possibleEntries.randomElement() ?? "Pong."
+    }
+    
+    /**
+        Gets a random response when the command `!qt` is run.
+     */
+    public func getQtMessage() -> String {
+        let possibleEntries = [
+            "As your chief, I command you to stop doing that.",
+            "Flattering, but knock it off.",
+            "Do you think it's funny to call me a Qt?",
+            "You aren't entertaining anyone with that."
+        ]
+        return possibleEntries.randomElement() ?? "I niot qt!"
+    }
+    
+    /**
+        Runs a command based off of a token.
+        - parameter command: The token to run as a command.
+        - parameter message: The message to act on.
+        - throws: Throws `DreemurrError.invalidCommand` when running into an invalid command.
+     */
     public func parseCommand(command: DreemurrCommands, message: Message) throws {
         switch command {
         case .help:
@@ -42,14 +90,18 @@ public class Alice: Dreemurr {
             - !ping - Ping the bot.
 
             **Bot Commands**
+            - !aboutme - Alias of '!introduce'.
             - !qt - Tell the bot she's a Qt.
             """))
         case .introduce:
             message.reply(with: self.introduceSelf())
         case .ping:
-            message.reply(with: "Pong.")
+            message.reply(with: getPingMessage())
         case .qt:
-            message.reply(with: "I niot qt!")
+            message.addReaction("🚫")
+            message.reply(with: getQtMessage())
+        case .aboutme:
+            message.reply(with: self.introduceSelf())
         default:
             throw DreemurrError.invalidCommand
         }
